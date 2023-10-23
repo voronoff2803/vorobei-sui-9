3dea40cc6910938301a4f8866f7dc295f8901b57
Alexey Voronov <voronoff2803@gmail.com>
2023-10-23 13:42:05 +0300

    done

--- vorobei-sui-9/ContentView.swift
+++ vorobei-sui-9/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var dragOffset: CGSize = .zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+            Canvas { context, size in
+                context.addFilter(.alphaThreshold(min: 0.5, color: .white))
+                context.addFilter(.blur(radius: 30))
+                context.drawLayer { ctx in
+                    for index in [1,2] {
+                        if let resolved = context.resolveSymbol(id: index) {
+                            ctx.draw(resolved, at: CGPoint(x: size.width / 2, y: size.height / 2))
+                        }
+                    }
+                }
+            } symbols: {
+                Circle()
+                    .fill(.white)
+                    .frame(width: 200, height: 200)
+                    .tag(1)
+                    
+                Circle()
+                    .fill(.white)
+                    .frame(width: 200, height: 200)
+                    .tag(2)
+                    .offset(dragOffset)
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged({ value in
+                        dragOffset = value.translation
+                    }) .onEnded({ _ in
+                        withAnimation {
+                            dragOffset = .zero
+                        }
+                    })
+            )
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
